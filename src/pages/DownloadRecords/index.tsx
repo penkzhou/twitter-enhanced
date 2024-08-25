b37b95e4457d7294060f6eafb27a4e0e7bac31a2
Peng Zhou <penkzhou@gmail.com>
2024-08-25 16:05:06 +0800

    add download record feature

--- src/pages/DownloadRecords/index.tsx
+++ src/pages/DownloadRecords/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import DownloadRecords from './DownloadRecords';
-import './index.css';
 
 const container = document.getElementById('app-container');
 
